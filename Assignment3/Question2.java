5933793a0caa281c14cbcdef72ca81cc6ed800b4
Aman Jain <amanjain162001@gmail.com>
2023-02-10 08:33:06 -0800

    initial commit

---
+++ Assignment3/Question2.java
@@ -0,0 +1,14 @@
+package Assignment3;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 1;
+while (i<=20) {
+	System.out.println(i);
+	i++;
+}
+	}
+
+}
