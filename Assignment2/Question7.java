4368cf19c10e4e9bfb5477edb007c5d7dc4afe01
unknown <amanjain162001@gmail.com>
2023-02-10 07:44:23 -0800

    Assignment2

---
+++ Assignment2/Question7.java
@@ -0,0 +1,15 @@
+package Assignment2;
+
+public class Question7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 10;
+int y = 20;
+System.out.println("var x = "+x);
+System.out.println("var y = "+y);
+System.out.println("smallest number = "+Math.min(x,y));
+System.out.println("Largest number = "+Math.max(x, y));
+	}
+
+}
