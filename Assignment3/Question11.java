4965379e6fb39eed888e300fad4a61472a55e249
Aman Jain <amanjain162001@gmail.com>
2023-02-14 08:45:41 -0800

    initial commit

---
+++ Assignment3/Question11.java
@@ -0,0 +1,19 @@
+package Assignment3;
+
+public class Question11 {
+
+	public static void main(String[] args) {
+		int num1 = 4;
+		
+		
+		switch(num1%2){
+		case 0:
+			System.out.println("number is even");
+			break;
+		case 1:
+			System.out.println("number is odd");
+		}
+
+	}
+
+}
---
+++ Assignment3/Question6.java
@@ -0,0 +1,16 @@
+package Assignment3;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 10;
+while(i<=100) {
+	if(i%2==0) {
+		System.out.println(i);
+	}
+	i++;
+}
+	}
+
+}
