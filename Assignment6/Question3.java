7c4aee082b17c5923ba51a532e7c422a1d0cae10
Aman Jain <amanjain162001@gmail.com>
2023-02-27 10:25:06 -0800

    initial commit

---
+++ Assignment6/Question3.java
@@ -0,0 +1,13 @@
+package Assignment6;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "aman";
+		
+		System.out.println(str.length());
+		
+	}
+
+}
---
+++ Assignment6/Question6.java
@@ -0,0 +1,15 @@
+package Assignment6;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//the first way to creating string
+		String str = "aman";
+		//the second way by object
+		String s1 = new String("aman");
+		System.out.println(str);
+		System.out.println(s1);
+	}
+
+}
