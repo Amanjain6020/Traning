affc8b2c28601e76acac78c21d0d0cddef1e021b
Aman Jain <amanjain162001@gmail.com>
2023-03-16 08:24:24 -0700

    initial commit

---
+++ package12/Question2.java
@@ -0,0 +1,19 @@
+package package12;
+class Question21{
+	Question21(){
+		
+		System.out.println("from default constructor");
+	}
+	Question21(int i){
+		System.out.println("from argumental constructor");
+	}
+}
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new Question21();
+new Question21(5);
+	}
+
+}
